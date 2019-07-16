4fda0e57567ad0a399b1d6f7850a247f7a03f790
Roman Sorin <rmaximsorin@gmail.com>
2019-07-16 18:25:32 -0400

    Landing page title, subtitle

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { Section, Container, Heading } from 'react-bulma-components/full';
+import { Section, Container, Heading, Hero, Columns } from 'react-bulma-components/full';
 
 class LandingPage extends Component {
     render() {
         return (
             <Section>
-                <Container>
-                    <Heading>Section</Heading>
-                    <Heading subtitle>
-                        A simple container to divide your page into <strong>sections</strong>, like the one you are currently
-                        reading
-          </Heading>
-                </Container>
-            </Section>
+                <Hero>
+                    <Hero.Body className="has-text-centered">
+                        <Container>
+                            <Columns mobile>
+                                <Columns.Column size="half" offset="one-quarter">
+                                    <Heading id="landing-page-heading"><span id="landing-page-heading-accented">what</span>stack</Heading>
+                                    <Heading subtitle size={3} className="has-text-weight-light">
+                                        Input your needs, Output your stack.<br />
+                                        Starting projects has<br />
+                                        never been simpler.
+                                    </Heading>
+                                </Columns.Column>
+                            </Columns>
+
+                        </Container>
+                    </Hero.Body>
+                </Hero>
+            </Section >
         );
     }
 }
