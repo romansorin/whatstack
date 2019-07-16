abc7b77b4e7e9e7ad36134e4817992a5fd1dca60
Roman Sorin <rmaximsorin@gmail.com>
2019-07-16 14:45:17 -0400

    Header component desktop

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { Level, Heading, Section, Container, Image } from 'react-bulma-components/full';
+import logo from './logo.svg';
+import './header.scss';
 
-export class Header extends Component {
+class Header extends Component {
     render() {
         return (
-            <header>
+            <Section renderAs="header">
+                <Container fluid>
+                    <Level renderAs="nav">
+                        <Level.Side align="left">
+                            <Level.Item>
+                                <Image src={logo} />
+                            </Level.Item>
+                        </Level.Side>
+                        <Level.Side align="right">
+                            <Level.Item>
+                                <a href="https://www.romansorin.com" className="header-links">FAQ</a>
+                            </Level.Item>
+                        </Level.Side>
+                    </Level>
+                </Container>
+            </Section>
 
-            </header>
         );
     }
 }
