99b5cbcf969b074eb3e23caa621cf4e9d0eb52a9
Roman Sorin <rmaximsorin@gmail.com>
2019-08-10 22:03:48 -0400

    Formatting/linting

--- src/AppFooter.jsx
+++ src/AppFooter.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import Footer from 'react-bulma-components/lib/components/footer';
-import Container from 'react-bulma-components/lib/components/container';
-import Content from 'react-bulma-components/lib/components/content';
+import React, { Component } from 'react'
+import Footer from 'react-bulma-components/lib/components/footer'
+import Container from 'react-bulma-components/lib/components/container'
+import Content from 'react-bulma-components/lib/components/content'
 
 class AppFooter extends Component {
-    render() {
-        return (
-            <Footer renderAs="footer">
-                <Container>
-                    <Content>
-                        <p>
-                            <span className="has-text-weight-semibold">whatstack</span> by <a href="https://www.romansorin.com">romansorin</a> -
-                            © 2019
-                        </p>
-                    </Content>
-                </Container>
-            </Footer>
-        );
-    }
+  render() {
+    return (
+      <Footer renderAs='footer'>
+        <Container>
+          <Content>
+            <p>
+              <span className='has-text-weight-semibold'>whatstack</span> by
+              <a href='https://www.romansorin.com'> romansorin </a>- © 2019
+            </p>
+          </Content>
+        </Container>
+      </Footer>
+    )
+  }
 }
 
-export default AppFooter;
+export default AppFooter
--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import Navbar from 'react-bulma-components/lib/components/navbar';
-import Container from 'react-bulma-components/lib/components/container';
-import Button from 'react-bulma-components/lib/components/button';
+import React, { Component } from 'react'
+import Navbar from 'react-bulma-components/lib/components/navbar'
+import Container from 'react-bulma-components/lib/components/container'
+import Button from 'react-bulma-components/lib/components/button'
 
-import logo from './assets/images/logo.svg';
+import logo from './assets/images/logo.svg'
 
 class Header extends Component {
-    render() {
-        return (
-            <header>
-                <Navbar className="header">
-                    <Container>
-                        <Navbar.Brand>
-                            <Navbar.Item renderAs="a" href="/">
-                                <img
-                                    src={logo}
-                                    className="header-logo"
-                                    alt="whatstack, a tool for finding your next stack"
-                                />
-                            </Navbar.Item>
-                        </Navbar.Brand>
-                        <Navbar.Container className="is-hidden-touch" position="end">
-                            <Navbar.Item renderAs="a" href="mailto:roman@romansorin.com">
-                                <Button>Contact</Button>
-                            </Navbar.Item>
-                        </Navbar.Container>
-                    </Container>
-                </Navbar>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header>
+        <Navbar className='header'>
+          <Container>
+            <Navbar.Brand>
+              <Navbar.Item renderAs='a' href='/'>
+                <img
+                  src={logo}
+                  className='header-logo'
+                  alt='whatstack, a tool for finding your next stack'
+                />
+              </Navbar.Item>
+            </Navbar.Brand>
+            <Navbar.Container className='is-hidden-touch' position='end'>
+              <Navbar.Item renderAs='a' href='mailto:roman@romansorin.com'>
+                <Button>Contact</Button>
+              </Navbar.Item>
+            </Navbar.Container>
+          </Container>
+        </Navbar>
+      </header>
+    )
+  }
 }
 
-export default Header;
+export default Header
